318f5f42a35359d3662d73c5a195ec446198aa65
Denny McFarlane <dmack1boss@gmail.com>
2026-02-12 17:32:08 -0600

    save changes

--- books.js
+++ books.js
@@ -1,25 +1,31 @@
 function renderBooks() {
   const booksWrapper = document.querySelector('books');
-}
-  booksWrapper.innerHTML =
-  <div class="book">
-  <figure class="book__img--wrapper">
-   <img class="book__img" src="assets/atomic habits.jpg" alt="">
-   </figure>
-    <div class="book__title">
-    Atomic Habits
-   </div>
-  <div class="book__ratings">
- <i class="fas fa-star"></i>
-   <i class="fas fa-star"></i>
-   <i class="fas fa-star"></i>
-  <i class="fas fa-star"></i>
-  <i class="fas fa-star-half-alt"></i>
-  </div>
-  <div class="book__price">
-  <span class="book__price--normal">$59.95</span> $14.95
-    </div>
-    </div>{
+
+  const books = getBooks();
+
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
+      <figure class="book__img--wrapper">
+        <img class="book__img" src="${book.url}" alt="">
+      </figure>
+      <div class="book__title">
+        ${book.title}
+      </div>
+      <div class="book__ratings">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <div class="book__price">
+        <span class="book__price--normal">$$${book.originalPrice}</span> $$${book.salePrice}
+      </div>
+    </div>`
+  }).join("");
+
+  booksWrapper.innerHTML = booksHtml;
+  console.log(booksHtml);
 }
 
 setTimeout(() => {
